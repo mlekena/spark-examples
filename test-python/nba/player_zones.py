9ce38c76e1809c1d0c870c3e5c99e2e13245d8fc
Theko Lekena <thekolekena@gmail.com>
2020-11-14 09:53:17 -0500

    init lab2

---
+++ test-python/nba/player_zones.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+
+import sys
+import argparse
+from operator import add
+
+from pyspark.sql import SparkSession
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+parser = argparse.ArgumentParser
+parser.add_argument(
+    "--data_file", help="Data file to read in must be specified using --data_file")
+parser.add_argument(
+    "--data_output", help="Data output path must be specified using --data_output")
+
+#  SHOT_DIST, CLOSEST_DEF_DIST, SHOT_CLOCK
+init_k_zones = [
+    [8, 12, 20],
+    [15, 9, 15],
+    [22, 6, 10],
+    [30, 3, 5]
+]
+
+
+def main():
+    columns_of_interest = ['player_name',
+                           'SHOT_DIST', 'CLOSEST_DEF_DIST', 'SHOT_CLOCK']
+    args = parser.parse_args()
+    spark = SparkSession.builder.appName("MostComfortableZones").getOrCreate()
+
+    # remove comma between quotes for string type csv fields making it hard to parse.
+    lines = spark.read.option("header", True).csv(args.file)\
+        .select(*columns_of_interest).rdd.map
+
+
+if __name__ == "__main__":
+    main()
---
+++ test-python/nba/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source ../../env.sh
+PROJECTNAME="nba_analysis"
+INPUTPATH="/${PROJECTNAME}/input"
+OUTPUTPATH="/${PROJECTNAME}/output"
+TESTDATA="test_data/data_fraction.csv"
+SRC="./player_zones.py"
+/usr/local/hadoop/bin/hdfs dfs -rm -r $INPUTPATH
+/usr/local/hadoop/bin/hdfs dfs -rm -r $OUTPUTPATH
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p $INPUTPATH
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal $TESTDATA $OUTPUTPATH
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 $SRC hdfs://$SPARK_MASTER:9000$INPUTPATH
