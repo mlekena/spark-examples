7d36abcf90d38db4f2370045495a55249a309b67
Theko Lekena <thekolekena@gmail.com>
2020-12-08 19:02:24 -0500

    add read data file code.

--- test-mllib/python/toxic-comments/run.sh
+++ test-mllib/python/toxic-comments/run.sh
@@ -5,4 +5,6 @@ PROJECTNAME="toxic_commment_classifier"
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /$PROJECTNAME/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_train.csv /$PROJECTNAME/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_test.csv /$PROJECTNAME/input/
-/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./$PROJECTNAME.py
+/usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 ./$PROJECTNAME.py \
+--test_data_file=.hdfs://$SPARK_MASTER:9000/input/sample_test.csv \
+--train_data_file=hdfs://$SPARK_MASTER:9000/input/sample_train.csv
--- test-mllib/python/toxic-comments/toxic_comment_classifier.py
+++ test-mllib/python/toxic-comments/toxic_comment_classifier.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 
 from pyspark.sql import SparkSession
@@ -5,6 +6,24 @@ import pyspark.sql.functions as sparkFunc
 import pyspark.sql.types as T
 from pyspark.ml.feature import Tokenizer, HashingTF, IDF
 from pyspark.ml.classification import LogisticRegression
+
+# Globally define to make available in functions. Instantiated in main
+context = None
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--test_data_file", help="Data file to read in must be specified using --test_data_file")
+parser.add_argument(
+    "--train_data_file", help="Data output path must be specified using --train_data_file", action="store_const", const=None)
+
+
+def to_spark_df(file_name):
+    assert(context), "calling to_spark_df without an instantiated sparksession context"
+    df = pd.read_csv(file_name)
+    df.fillna("", inplace=True)
+    df = context.createDataFrame(df)
+    return (df)
+
 
 if __name__ == "__main__":
 
@@ -22,4 +41,8 @@ if __name__ == "__main__":
                .getOrCreate())
 
     context.sparkContext.setLogLevel("INFO")
-    
+    args = parser.parse_args()
+
+    train_data = to_spark_df(args.train_data_file)
+    test_data = to_spark_df(args.test_data_file)
+    train_data.show()
