c094dd021fa4dba3b929179ae5c7e0e4d618ef6a
Theko Lekena <mlekena@fordham.edu>
2020-12-16 01:29:14 +0000

    fix run script

--- test-mllib/python/toxic-comments/run.sh
+++ test-mllib/python/toxic-comments/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 source ../../../env.sh
 PROJECTNAME="toxic_comment_classifier"
-FULLDATAPATH=user/root/data/$PROJECTNAME/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r FULLDATAPATH
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p FULLDATAPATH
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_train.csv FULLDATAPATH
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_test.csv FULLDATAPATH
-/usr/local/hadoop/bin/hdfs dfs -ls FULLDATAPATH
+FULLDATAPATH=/$PROJECTNAME/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r $FULLDATAPATH
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p $FULLDATAPATH
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_train.csv $FULLDATAPATH
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ./sample_test.csv $FULLDATAPATH
+/usr/local/hadoop/bin/hdfs dfs -ls $FULLDATAPATH
 echo "#########################################################################################"
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 $PROJECTNAME.py \
---test_data_file=data/$PROJECTNAME/input/sample_test.csv \
---train_data_file=data/$PROJECTNAME/input/sample_train.csv
+--test_data_file=hdfs://$SPARK_MASTER:9000/$PROJECTNAME/input/sample_test.csv \
+--train_data_file=hdfs://$SPARK_MASTER:9000/$PROJECTNAME/input/sample_train.csv
